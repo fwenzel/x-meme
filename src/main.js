b93670b71401c66ee2fa91a93bb1865d37c6fa8d
Fred Wenzel <fwenzel@mozilla.com>
2014-03-07 16:42:40 -0800

    WIP: A Fry meme.

--- src/main.js
+++ src/main.js
@@ -1,23 +1,34 @@
-(function(){  
+(function(){
 
-  // Create your component here
-  // http://x-tags.org/docs
-  
-  xtag.register('x-stub', {
+  xtag.register('x-meme', {
     lifecycle: {
       created: function() {},
-      inserted: function() {},
+      inserted: function() {
+        console.log(this);
+        if (!(this.hasAttribute('type') && this.getAttribute('type') == 'futurama-fry')) return;
+
+        var container = document.createElement('div');
+        var img = document.createElement('img');
+        img.src = 'http://www.quickmeme.com/memes/334.jpg';
+
+        container.appendChild(img);
+        while (this.firstChild) {
+          container.appendChild(this.firstChild);
+        }
+        this.appendChild(container);
+      },
+
       removed: function() {},
       attributeChanged: function() {}
-    }, 
-    events: { 
-    
+    },
+    events: {
+
     },
     accessors: {
-      
-    }, 
+
+    },
     methods: {
-      
+
     }
   });
 
