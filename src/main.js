da9f3a3bedada7a84a32dc86eb51759aac20725a
Fred Wenzel <fwenzel@mozilla.com>
2014-03-11 16:42:20 -0700

    Semantic header/footer and attribute/property support

--- src/main.js
+++ src/main.js
@@ -2,20 +2,28 @@
 
   xtag.register('x-meme', {
     lifecycle: {
-      created: function() {},
-      inserted: function() {
-        console.log(this);
-        if (!(this.hasAttribute('type') && this.getAttribute('type') == 'futurama-fry')) return;
+      created: function() {
+        var xtag = this.xtag;
 
         var container = document.createElement('div');
-        var img = document.createElement('img');
-        img.src = 'http://www.quickmeme.com/memes/334.jpg';
 
+        var img = document.createElement('img');
         container.appendChild(img);
-        while (this.firstChild) {
-          container.appendChild(this.firstChild);
-        }
+        xtag.img = img;
+
+        ['header', 'footer'].forEach(function(caption) {
+          var captElem = document.createElement(caption);
+          container.appendChild(captElem);
+          xtag[caption] = captElem;
+          console.log(xtag);
+        });
+
         this.appendChild(container);
+      },
+
+      inserted: function() {
+        if (!(this.hasAttribute('type') && this.getAttribute('type') == 'futurama-fry')) return;
+        this.xtag.img.src = 'http://www.quickmeme.com/memes/334.jpg';
       },
 
       removed: function() {},
@@ -25,7 +33,20 @@
 
     },
     accessors: {
+      header: {
+        attribute: {},
 
+        set: function(text) {
+          this.xtag.header.textContent = text;
+        }
+      },
+      footer: {
+        attribute: {},
+
+        set: function(text) {
+          this.xtag.footer.textContent = text;
+        }
+      }
     },
     methods: {
 
