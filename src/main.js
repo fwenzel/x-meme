96832c3a78300c790fe89e409b8baff8c20ea7cd
Fred Wenzel <fwenzel@mozilla.com>
2014-03-15 22:24:44 -0700

    First shot at an x-doge tag. Issue #5.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 (function(){
+  var img_baseurl = 'http://fwenzel.github.io/x-meme/img/memes/';
   var memes = {
     'bad-luck-brian': {},
     'condescending-wonka': {},
@@ -11,6 +12,7 @@
     'scumbag-steve': {},
     'success-kid': {}
   };
+
 
   xtag.register('x-meme', {
     lifecycle: {
@@ -63,14 +65,79 @@
           if (memes[memetype]) {  // Valid meme.
             var ext = memes[memetype]['filetype'] || 'jpg';
             this.removeAttribute('src');
-            this.xtag.img.src = 'http://fwenzel.github.io/x-meme/img/memes/' + memetype + '.' + ext;
+            this.xtag.img.src = img_baseurl + memetype + '.' + ext;
           } else if (memetype !== 'custom') {
             this.xtag.img.src = '';
           }
         }
       }
     }
+  });
 
+
+  xtag.register('x-doge', {
+    lifecycle: {
+      created: function() {
+        var xtag = this.xtag;
+
+        var container = document.createElement('div');
+        xtag.container = container;
+
+        var img = document.createElement('img');
+        container.appendChild(img);
+        xtag.img = img;
+
+        // Default to doge background.
+        if (!this.hasAttribute('src')) {
+          this.type = 'doge';
+        }
+
+        this.appendChild(container);
+      }
+    },
+
+    accessors: {
+      src: {
+        attribute: {},
+
+        set: function(src) {
+          this.setAttribute('type', 'custom');
+          this.xtag.img.src = src;
+        }
+      },
+      phrases: {
+        attribute: {},
+
+        set: function(phrases) {
+          var cont = this.xtag.container;
+
+          // Remove old phrases.
+          for (var p of cont.querySelectorAll('.phrase')) {
+            p.parentNode.removeChild(p);
+          }
+
+          // Add new phrases.
+          phrases.split('|').forEach(function(phrase) {
+            var p = document.createElement('span');
+            p.className = 'phrase';
+            p.textContent = phrase;
+            cont.appendChild(p);
+          });
+        }
+      },
+      type: {
+        attribute: {},
+
+        set: function(memetype) {
+          if (memetype === '' || memetype === 'doge') {
+            this.removeAttribute('src');
+            this.xtag.img.src = img_baseurl + 'doge.jpg';
+          } else if (memetype !== 'custom') {
+            this.xtag.img.src = '';
+          }
+        }
+      }
+    }
   });
 
 })();
